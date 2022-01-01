881bc46dd09768843497aada1ead88bfa95623e9
khk811 <hyunkyungkim811@gmail.com>
2022-01-01 14:00:18 +0900

    use write instead of printf

--- get_next_line/test/test.c
+++ get_next_line/test/test.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
-#include <stdio.h>
+
+typedef struct s_list
+{
+    void    *content;
+    struct s_list   *next;
+}   t_list;
 
 char    *get_next_line(int fd)
 {
@@ -22,14 +28,26 @@ char    *get_next_line(int fd)
 int main()
 {
     int target_fd;
-    char    *result;
+    t_list  **result;
+    t_list  *element;
+    t_list  *new;
 
     target_fd = open("./read_fd_target", O_RDONLY);
-    result = get_next_line(target_fd);
-    while (result)
+    element = malloc(sizeof(t_list));
+    element->content = "";
+    element->next = NULL;
+    result = &element;
+    while (element)
     {
-        printf("%s", result);
-        result = get_next_line(target_fd);
+        new = malloc(sizeof(t_list));
+        new->content = get_next_line(target_fd);
+        if (!(new->content))
+            break ;
+        while (*(char *)(new->content))
+            write(1, (new->content)++, 1);
+        new->next = NULL;
+        element->next = new;
+        element = element->next;
     }
     return (0);
 }
