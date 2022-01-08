35edb1c10e31c2fe55f426b3b8377bacd05dd245
khk811 <hyunkyungkim811@gmail.com>
2022-01-08 22:38:07 +0900

    rm add_lst test line

--- get_next_line/mirror_dimension/main.c
+++ get_next_line/mirror_dimension/main.c
@@ -6,22 +6,8 @@
 int	main()
 {
 	int	target_fd;
-	int	i;
-	t_list	*head;
-	t_list	element2 = {"BBBB", NULL};
-	t_list	element1 = {"AAAA", &element2};
-	t_list	element3 = {"CCCC", NULL};
 
-	i = 0;
 	target_fd = open("./short_target", O_RDONLY);
 	printf("gnl result: %s", get_next_line(target_fd));
-	head = &element1;
-	add_element(&head, &element3);
-	printf("\n =====%s\n", head->next->content);
-	while (head->next)
-	{
-		printf("##%s\n", head->content);
-		head = head->next;
-	}
 	return (0);
 }
