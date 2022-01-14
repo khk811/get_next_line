f59f70c2f64a0fede2e3f25b53d2827ed905ec23
khk811 <hyunkyungkim811@gmail.com>
2022-01-14 13:42:59 +0900

    add strcat func.

--- get_next_line/mirror_dimension/get_next_line.c
+++ get_next_line/mirror_dimension/get_next_line.c
@@ -3,16 +3,26 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+static int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
 static char	*alloc_arr(char **arr, int size)
 {
-	if (*arr)
+	if (!*arr)
 	{
 		*arr = (char *)malloc(sizeof(char) * (size + 1));
 		if (!*arr)
 			return (NULL);
 	}
 	/*
-	else if (arr)
+	else if (*arr)
 	{
 	}*/
 	return (*arr);
@@ -32,13 +42,35 @@ static int	has_a_nl(char *s)
 	return (0);
 }
 
+static char	*cat_a_line(char *dst, char *src, int flag)
+{
+	int	i;
+	int	j;
+
+	i = ft_strlen(dst);
+	j = 0;
+	while (src[j])
+	{
+		if (src[j] == '\n' && flag == 1)
+		{
+			dst[i++] = src[j];
+			break ;
+		}
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
 static int	valid_read(int fd, char **buf)
 {
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (0);
 	if (!alloc_arr(buf, BUFFER_SIZE))
 		return (0);
-	if (read(fd, *buf,BUFFER_SIZE) <= 0)
+	if (read(fd, *buf, BUFFER_SIZE) <= 0)
 		return (0);
 	(*buf)[BUFFER_SIZE] = '\0';
 	return (1);
@@ -47,14 +79,25 @@ static int	valid_read(int fd, char **buf)
 char	*get_next_line(int fd)
 {
 	char	*buf;
-	//char	*result;
+	char	*result;
 	//static char	*save;
 
+	result = NULL;
+	buf = NULL;
 	if (!valid_read(fd, &buf))
 		return (NULL);
+	alloc_arr(&result, ft_strlen(buf));
+	result[ft_strlen(buf)] = '\0';
+	cat_a_line(result, buf, 0);
+	has_a_nl(buf);
+	/*
 	while (!has_a_nl(buf))
+	{
+		alloc_arr(result, ft_strlen(buf));
+		cat_a_line(result, buf, 0);
 		valid_read(fd, &buf);
-	return (buf);
+	}*/
+	return (result);
 }
 
 int	main()
