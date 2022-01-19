122f331cc18657258c340312c346c925b4c6457a
khk811 <hyunkyungkim811@gmail.com>
2022-01-19 21:36:34 +0900

    working bonus

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,138 @@
+#include "get_next_line_bonus.h"
+#include <stdio.h>
+
+static char	*read_until(char **buf, char **result)
+{
+	int	i;
+	int	j;
+
+	j = ft_strlen(*result);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			(*result)[j] = (*buf)[i++];
+			alloc_arr(result, 0);
+			if ((*buf)[i])
+				ft_memmove((*buf), (*buf) + i, ft_strlen((*buf) + i) + 1);
+			else if (*buf)
+			{
+				ft_memset(*buf, 0, ft_strlen(*buf) + 1);
+				free(*buf);
+				*buf = NULL;
+			}
+			return (*result);
+		}
+		(*result)[j++] = (*buf)[i++];
+	}
+	return (NULL);
+}
+
+static t_list	*find_fd(t_list **lst, int fd)
+{
+	t_list	*tmp;
+
+	tmp = *lst;
+	if (!*lst || !lst)
+		return (NULL);
+	if (tmp->fd == fd)
+	{
+		return (tmp);
+	}
+	while (tmp->next)
+	{
+		if (tmp->fd == fd)
+			break ;
+		tmp = tmp->next;
+	}
+	if (tmp->fd != fd)
+		return (NULL);
+	return (tmp);
+}
+
+static void	rm_element(t_list **lst, int fd)
+{
+	t_list	*tmp;
+	t_list	*before;
+
+	before = NULL;
+	tmp = *lst;
+	if (!*lst)
+		return ;
+	if (!tmp->next)
+	{
+		free(*lst);
+		*lst = NULL;
+		lst = NULL;
+		return ;
+	}
+	while (tmp->next && tmp->fd != fd)
+	{
+		before = tmp;
+		tmp = tmp->next;
+	}
+	if (before)
+		before->next = tmp->next;
+	if (!before)
+		*lst = tmp->next;
+	free(tmp);
+	tmp = NULL;
+}
+
+static void	add_new_element(t_list **lst, int fd, char *buf)
+{
+	t_list	*tmp;
+	t_list	*new;
+
+	tmp = find_fd(lst, fd);
+	if (tmp)
+	{
+		tmp->buf = buf;
+		return ;
+	}
+	tmp = *lst;
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return ;
+	new->fd = fd;
+	new->buf = buf;
+	new->next = NULL;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*fd_lst = NULL;
+	       t_list	*matching_fd;
+	         char	*buf;
+	         char	*result;
+
+	result = NULL;
+	buf = NULL;
+	matching_fd = find_fd(&fd_lst, fd);
+	if (matching_fd)
+		buf = matching_fd->buf;
+	if (!buf)
+		valid_read(fd, &buf);
+	while (buf)
+	{
+		if (!alloc_arr(&result, ft_strlen(buf)))
+			break ;
+		if (read_until(&buf, &result))
+			break ;
+		valid_read(fd, &buf);
+	}
+	if (find_fd(&fd_lst, fd) && !buf)
+		rm_element(&fd_lst, fd);
+	else if (buf)
+		add_new_element(&fd_lst, fd, buf);
+	return (result);
+}
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct	s_list
+{
+	int	fd;
+	char	*buf;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+char	*alloc_arr(char **arr, int size);
+void	*ft_memset(void *b, int c, int len);
+void	*ft_memmove(void *dst, void *src, int len);
+int	valid_read(int fd, char **buf);
+int		ft_strlen(char *s);
+
+#endif
